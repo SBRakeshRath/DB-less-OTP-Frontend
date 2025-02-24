af5f13afee184c9cff50834a191a2c9e6dba2d21
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2025-02-25 01:01:03 +0530

    added blog section

--- src/components/nav.component.vue
+++ src/components/nav.component.vue
@@ -7,6 +7,7 @@ header {
   nav {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 0.2rem 0.5rem;
     background-color: #dadde2;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
@@ -14,6 +15,19 @@ header {
   .left {
     img {
       width: 100px;
+    }
+  }
+  .right {
+    a {
+      text-decoration: none;
+      padding: 10px 20px;
+      color: #000;
+      background-color: #ffa500;
+      /* margin-right: 15px; */
+      border-radius: 5px;
+      &:hover {
+        background-color: #ffbb3b;
+      }
     }
   }
 }
@@ -24,6 +38,12 @@ header {
     <nav>
       <div class="left">
         <img :src="logo" alt="logo-image" />
+      </div>
+      <div class="right">
+        <a
+          href="https://medium.com/@sbrakeshrath/database-less-otp-a-concept-b73100fec797"
+          >Blog</a
+        >
       </div>
     </nav>
   </header>
